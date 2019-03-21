a7ea593b39a83dcc1fbb6e301dd193f6f51968c8
Oleksandr Andriienko <oandriie@redhat.com>
2019-03-21 14:12:49 +0200

    Fix up.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ printf "${BLUE}Building service (docker image)${NC}\n"
 
 docker build --no-cache -t eclipse/che-machine-exec:latest .
 GIT_HASH=$(git rev-parse --short=7 HEAD)
-docker tag eclipse/che-machine-exec:latest aandrienko/che-machine-exec:${GIT_HASH}
+docker tag eclipse/che-machine-exec:latest eclipse/che-machine-exec:${GIT_HASH}
 
 printf "${BLUE}Generating Che plug-in file...${NC}\n"
 cd ${DIR}/assembly && ./build.sh
