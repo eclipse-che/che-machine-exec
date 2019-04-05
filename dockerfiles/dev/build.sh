e00e6b82d949a43ed6be08b90330c927f3b4f693
Oleksandr Andriienko <oandriie@redhat.com>
2019-04-05 17:31:19 +0300

    Add che devfile and dockerfile for development.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

---
+++ dockerfiles/dev/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Copyright (c) 2018-2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+docker build -t aandrienko/che-machine-exec-dev:latest .
