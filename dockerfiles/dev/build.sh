5e31c68a91f53d7b795be24b914d790c3fb9cd55
Oleksandr Andriienko <oandriie@redhat.com>
2019-04-09 11:01:23 +0000

    Address review changes. Add less to the dev image.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- dockerfiles/dev/build.sh
+++ dockerfiles/dev/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #
-<<<<<<< HEAD
 # Copyright (c) 2018-2019 Red Hat, Inc.
-=======
-# Copyright (c) 2012-2019 Red Hat, Inc.
->>>>>>> 7a3151e5eac94ad414b3c1ffeeb6fc2340b035a8
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -12,4 +8,4 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
-docker build -t aandrienko/che-machine-exec-dev:latest .
+docker build -t eclipse/che-machine-exec-dev:latest .
