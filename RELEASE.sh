db9203bb23da98d3ea7d9a097b437676f2768b2b
nickboldt <nboldt@redhat.com>
2020-01-07 12:48:41 -0500

    tweak script comments
    Change-Id: Ice550db464ee7100fbfadfae768e42179a9ec2b8
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- RELEASE.sh
+++ RELEASE.sh
@@ -11,7 +11,7 @@ TRIGGER_RELEASE=0
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     '-t'|'--trigger-release') TRIGGER_RELEASE=1; shift 0;;
-	'-r'|'--repo') REPO="$2"; shift 1;;
+    '-r'|'--repo') REPO="$2"; shift 1;;
     '-v'|'--version') VERSION="$2"; shift 1;;
   esac
   shift 1
@@ -20,43 +20,47 @@ done
 usage ()
 {
     echo "Usage: $0 --repo [GIT REPO TO EDIT] --version [VERSION TO RELEASE] [--trigger-release]"
-    echo "Example: $0 --repo git@github.com:eclipse/che-machine-exec --version 7.7.0 --trigger-release"
+    echo "Example: $0 --repo git@github.com:eclipse/che-machine-exec --version 7.7.0 --trigger-release"; echo
 }
+
 if [[ ! ${VERSION} ]] || [[ ! ${REPO} ]]; then
   usage
   exit 1
 fi
 
+# derive branch from version
 BRANCH=${VERSION%.*}.x
 
+# work in tmp dir
 TMP=$(mktemp -d); mkdir -p $TMP; cd $TMP 
 
-# get sources
+# get sources from master branch
 echo "Check out ${REPO} to ${TMP}/${REPO##*/}"
 git clone ${REPO} -q
 cd ${REPO##*/}
 git fetch origin master:master
 git checkout master
 
-# create new branch
+# create new branch off master + push to origin
 git branch ${BRANCH} || git checkout ${BRANCH} && git pull origin ${BRANCH}
 git push origin ${BRANCH}
 git fetch origin ${BRANCH}:${BRANCH}
 git checkout ${BRANCH}
 
-# change VERSION file + commit change
+# change VERSION file + commit change into .x branch
 echo ${VERSION} > VERSION
 git commit -s -m "[release] Bump to ${VERSION} in ${BRANCH}" VERSION
 git pull origin ${BRANCH}
 git push origin ${BRANCH}
 
-# push new branch to release branch
 if [[ $TRIGGER_RELEASE -eq 1 ]]; then
+    # push new branch to release branch to trigger CI build
     git fetch origin ${BRANCH}:${BRANCH}
     git checkout ${BRANCH}
     git branch release -f 
     git push origin release -f
 
+    # tag the release
     git checkout ${BRANCH}
     git tag ${VERSION}
     git push origin ${VERSION}
@@ -66,7 +70,7 @@ fi
 git fetch origin master:master
 git checkout master
 
-# change VERSION file + commit change
+# change VERSION file + commit change into master branch
 [[ $BRANCH =~ ^([0-9]+)\.([0-9]+).x ]] && BASE=${BASH_REMATCH[1]}; NEXT=${BASH_REMATCH[2]}; let NEXT=NEXT+1 # for BRANCH=7.10.x, get BASE=7, NEXT=11
 echo "${BASE}.${NEXT}.0-SNAPSHOT" > VERSION
 BRANCH=master
@@ -74,6 +78,5 @@ git commit -s -m "[release] Bump to ${BASE}.${NEXT}.0-SNAPSHOT in ${BRANCH}" VER
 git pull origin ${BRANCH}
 git push origin ${BRANCH}
 
-# cleanup temp
+# cleanup tmp dir
 cd /tmp && rm -fr $TMP
-
