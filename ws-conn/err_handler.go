abf3dd755fbecc7d40e9375546cf37af94f19fc6
Oleksandr Andriienko <oandriie@redhat.com>
2020-05-10 01:12:04 +0300

    Close attach websocket connection on exec exit. Add pong handler. Stop activity tracker on exec exit. (#96)
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- ws-conn/err_handler.go
+++ ws-conn/err_handler.go
@@ -18,7 +18,7 @@ import (
 
 // Return true if err is normal close connection error(connection close client). Return false otherwise.
 // Note: In case if connection was close normally by client it's ok for us, we should not log error.
-func IsNormalWSError(err error) bool {
+func IsClosedByClientError(err error) bool {
 	normalCloseCodes := []int{ws.CloseGoingAway, ws.CloseNormalClosure, ws.CloseNoStatusReceived}
 	if closeErr, ok := err.(*ws.CloseError); ok {
 		for _, code := range normalCloseCodes {
