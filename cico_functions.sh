da4897c6f41a2618ce4b66a81d6904761f62b3fd
Prabhav Thali <Prabhav.Thali1@ibm.com>
2020-06-25 16:03:30 +0530

    Using tagged image for qemu
    Signed-off-by: Prabhav Thali <Prabhav.Thali1@ibm.com>

--- cico_functions.sh
+++ cico_functions.sh
@@ -75,7 +75,7 @@ function install_deps() {
 
   #Enable qemu and binfmt support
   docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
-  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  docker run --rm --privileged multiarch/qemu-user-static:4.2.0-7 --reset -p yes
   
   echo 'CICO: Dependencies installed'
 }
