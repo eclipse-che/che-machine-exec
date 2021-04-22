d53d8144dbebb0f674929ce49e319aab0e152315
Oleksandr Andriienko <oandriie@redhat.com>
2021-04-22 13:46:56 +0300

    Fix compilation.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- auth/user.go
+++ auth/user.go
@@ -16,7 +16,6 @@ import (
 	"context"
 	"errors"
 
-	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
