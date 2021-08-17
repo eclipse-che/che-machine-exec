8b1554e0cfcb4025cd0cff0cf323827d3cfc0221
nickboldt <nboldt@redhat.com>
2021-08-17 13:24:46 -0300

    merge in changes from theia implementation; don't use TRAVIS_TAG (aka SUFFIX); just append -travis on SHORT_SHA variable; don't run shell scripts in a nested bash (just run them as is)
    Change-Id: I04dce67f8fafccb3da9cdf411f79cfdcd9a49098
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- .travis/publish_multiarch.sh
+++ .travis/publish_multiarch.sh
@@ -9,20 +9,31 @@
 #
 # Contributors:
 #   Red Hat, Inc. - initial API and implementation
+#
+# Create amend with images built on individual architectures
+
 set -e
 
-# Create amend with images built on individual architectures
-AMEND=""
-AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-amd64";
-AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-arm64";
-AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-ppc64le";
-AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-s390x";
+PUBLISH_IMAGES_LIST=(
+  che-machine-exec
+)
+for image in "${PUBLISH_IMAGES_LIST[@]}"; do
+    the_image="${REGISTRY}/${ORGANIZATION}/${image}"
+    AMEND=""
+    AMEND+=" --amend ${the_image}:${TAG}-amd64";
+    AMEND+=" --amend ${the_image}:${TAG}-arm64";
+    AMEND+=" --amend ${the_image}:${TAG}-ppc64le";
+    AMEND+=" --amend ${the_image}:${TAG}-s390x";
 
-# Create manifest and push multiarch image
-docker manifest create "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}" $AMEND
-docker manifest push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}"
-
-if [[ "$TAG" == "next" ]]; then
-    docker manifest create "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA}-${TRAVIS_TAG}" $AMEND
-    docker manifest push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA}-${TRAVIS_TAG}"
-fi
+    # Create manifest and push multiarch image
+    docker manifest create "${the_image}:${TAG}" "$AMEND"
+    docker manifest push "${the_image}:${TAG}"
+    
+    if [[ "${TAG}" == "next-travis" ]]; then
+       docker manifest create "${the_image}:${SHORT_SHA}" "$AMEND"
+       docker manifest push "${the_image}:${SHORT_SHA}"
+    else 
+       docker manifest create "${the_image}:latest" "$AMEND"
+       docker manifest push "${the_image}:latest"
+    fi
+done
