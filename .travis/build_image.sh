3edfc83f194390d91c5150aec763c7376905c814
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-08-20 17:41:32 +0530

    changes to sync with PR #170

--- .travis/build_image.sh
+++ .travis/build_image.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+#
+# Copyright (c) 2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
 set -e
 
-# Build images
-docker build -f build/dockerfiles/Dockerfile -t "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-${TRAVIS_CPU_ARCH}" .
-docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-${TRAVIS_CPU_ARCH}"
+#shellcheck disable=SC1073 disable=SC2066 disable=SC2153
+for image in ${IMAGE}; do
+    the_image="${REGISTRY}/${ORGANIZATION}/${image}"
 
-# Tag image with short_sha in case of next build
-if [[ "$TAG" == "next" ]]; then
-    docker tag "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-${TRAVIS_CPU_ARCH}" "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA}-${TRAVIS_TAG}-${TRAVIS_CPU_ARCH}"
-    docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA}-${TRAVIS_TAG}-${TRAVIS_CPU_ARCH}"
-fi
+    # Build images
+    docker build -f build/dockerfiles/Dockerfile -t "${the_image}:${TAG}-${TRAVIS_CPU_ARCH}" .
+    docker push "${the_image}:${TAG}-${TRAVIS_CPU_ARCH}"
+
+    # Tag image with short_sha in case of next build
+    if [[ "$TAG" == "next-travis" ]]; then
+        docker tag "${the_image}:${TAG}-${TRAVIS_CPU_ARCH}" "${the_image}:${SHORT_SHA}-${TRAVIS_CPU_ARCH}"
+        docker push "${the_image}:${SHORT_SHA}-${TRAVIS_CPU_ARCH}"
+    fi
+done
--- .travis/publish_multiarch.sh
+++ .travis/publish_multiarch.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
+#
+# Copyright (c) 2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+# Create amend with images built on individual architectures
+
 set -e
 
-# Create amend with images built on individual architectures
-AMEND=""
-AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-amd64";
-AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-arm64";
-AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-ppc64le";
-AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}-s390x";
+PUBLISH_IMAGES_LIST=(
+  che-machine-exec
+)
+for image in "${PUBLISH_IMAGES_LIST[@]}"; do
+    the_image="${REGISTRY}/${ORGANIZATION}/${image}"
+    AMEND=""
+    AMEND+=" --amend ${the_image}:${TAG}-amd64";
+    AMEND+=" --amend ${the_image}:${TAG}-arm64";
+    AMEND+=" --amend ${the_image}:${TAG}-ppc64le";
+    AMEND+=" --amend ${the_image}:${TAG}-s390x";
 
-# Create manifest and push multiarch image
-docker manifest create "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}" $AMEND
-docker manifest push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-${TRAVIS_TAG}"
+    # Create manifest and push multiarch image
+    eval docker manifest create "${the_image}:${TAG}" "$AMEND"
+    docker manifest push "${the_image}:${TAG}"
 
-if [[ "$TAG" == "next" ]]; then
-    docker manifest create "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA}-${TRAVIS_TAG}" $AMEND
-    docker manifest push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA}-${TRAVIS_TAG}"
-fi
+    if [[ "${TAG}" == "next-travis" ]]; then
+       eval docker manifest create "${the_image}:${SHORT_SHA}" "$AMEND"
+       docker manifest push "${the_image}:${SHORT_SHA}"
+    else 
+       eval docker manifest create "${the_image}:latest-travis" "$AMEND"
+       docker manifest push "${the_image}:latest-travis"
+    fi
+done
