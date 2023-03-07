978ea14f6cec66f3849b7295274c01e23023560e
Mykhailo Kuznietsov <mkuznets@redhat.com>
2023-03-07 14:10:28 +0200

    Set provenance:false for buildx builds
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -47,7 +47,9 @@ fi
 releaseMachineExec() {
   # docker buildx includes automated push to registry, so build using tag we want published, not just local ${IMAGE}
   docker buildx build \
-    --tag "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}" --push \
+    --tag "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}" \
+    --push \
+    --provenance=false \
     -f ./${DOCKERFILE} . --platform "linux/amd64,linux/ppc64le,linux/arm64" | cat
   echo "Pushed ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}"
 }
