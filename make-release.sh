7c6c9c754a10ffd8772f79d16973c585fd645b64
Mykhailo Kuznietsov <mkuznets@redhat.com>
2023-03-08 16:40:44 +0200

    Set provenance:false for buildx builds (#246)
    * Set provenance:false for buildx builds
    
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>
    
    * update copyright/ remove obsolete travis stuff
    
    * bump copyright
    
    ---------
    
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>
    Co-authored-by: Nick Boldt <nboldt@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2021 Red Hat, Inc.
+# Copyright (c) 2019-2023 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -47,7 +47,9 @@ fi
 releaseMachineExec() {
   # docker buildx includes automated push to registry, so build using tag we want published, not just local ${IMAGE}
   docker buildx build \
-    --tag "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}" --push \
+    --tag "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}" \
+    --push \
+    --provenance=false \
     -f ./${DOCKERFILE} . --platform "linux/amd64,linux/ppc64le,linux/arm64" | cat
   echo "Pushed ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}"
 }
